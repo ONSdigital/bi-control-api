9af431fc2bf25bbd677314ad4342735f78894bc5
Badur Chohan <chohab@MacBook-Pro.local>
2017-10-27 12:36:02 +0100

    removed unused methods

--- app/controllers/v1/SearchController.scala
+++ app/controllers/v1/SearchController.scala
@@ -15,12 +15,6 @@ import services._
  */
 class SearchController @Inject() (data: DataAccess, val config: Config) extends Controller {
 
-  //  def getBusiness(period: String, ubrn: String): Action[AnyContent] = {
-  //    Action.async { implicit request =>
-  //      proxyRequest()
-  //    }
-  //  }
-
   def getBusiness(period: String, id: String): Action[AnyContent] = {
     Action.async { implicit request =>
       proxyRequest(period, id)
@@ -32,22 +26,4 @@ class SearchController @Inject() (data: DataAccess, val config: Config) extends 
     val exitCode = data.getOutput(period: String, id: String)
     Ok(s"last, ${exitCode}").future
   }
-
-  //  def getBusiness(period: String, ubrn: String): Action[AnyContent] = {
-  //    Action.async { implicit request =>
-  //      Try(periodToYearMonth(period)) match {
-  //        case Success(validPeriod) => Try(data.getBusinessByIdForPeriod(validPeriod, ubrn)) match {
-  //          case Success(results) => resultsMatcher(results, ubrn)
-  //          case Failure(e) => InternalServerError(errAsJson(INTERNAL_SERVER_ERROR, "Internal Server Error", s"An error has occurred, please contact the server administrator $e")).future
-  //        }
-  //        case Failure(_: DateTimeException) => UnprocessableEntity(errAsJson(UNPROCESSABLE_ENTITY, "Unprocessable Entity", "Please ensure the period is in the following format: YYYYMM")).future
-  //        case _ => InternalServerError(errAsJson(INTERNAL_SERVER_ERROR, "Internal Server Error", s"An error has occurred, please contact the server administrator")).future
-  //      }
-  //    }
-  //  }
-
-  //  def resultsMatcher(results: List[Business], ubrn: String): Future[Result] = results match {
-  //    case Nil => NotFound(errAsJson(404, "Not Found", s"Could not find ubrn:  $ubrn")).future
-  //    case x => Ok(Business.toJson(x)).future
-  //  }
 }
