f8c35f41f90078de0f95dba953ec19f58f01af40
Badur Chohan <chohab@MacBook-Pro.local>
2017-11-07 13:31:35 +0000

    Added implicit for optional string

--- app/models/Business.scala
+++ app/models/Business.scala
@@ -7,35 +7,27 @@ import play.api.libs.json.{ JsValue, Json, OFormat }
 import utils.Utilities._
 
 case class Business(
-  //  id: Long, //ubrn
-  //  businessName: String,
-  //  industryCode: Option[String],
-  //  legalStatus: Option[String],
-  //  tradingStatus: Option[String],
-  //  turnover: Option[String],
-  //  employmentBands: Option[String],
-  //  postCode: Option[String],
-  //  vatRefs: Option[Seq[Long]],
-  //  payeRefs: Option[Seq[String]],
-  //  companyNo: Option[String]
-  id: String, //ubrn
+  id: Long, //ubrn
   businessName: String,
-  industryCode: String,
-  legalStatus: String,
-  tradingStatus: String,
-  turnover: String,
-  employmentBands: String,
-  postCode: String,
-  vatRefs: String,
-  payeRefs: String,
-  companyNo: String
+  industryCode: Option[String],
+  legalStatus: Option[String],
+  tradingStatus: Option[String],
+  turnover: Option[String],
+  employmentBands: Option[String],
+  postCode: Option[String],
+  vatRefs: Option[String],
+  payeRefs: Option[String],
+  companyNo: Option[String]
 )
 
 object Business {
   implicit val unitFormat: OFormat[Business] = Json.format[Business]
+
+  implicit def string2Option(s: String): Some[String] = Some(s)
+
   def toJson(business: String): Business = {
     val jsonMap = parseFull(business)
     val bv = hbaseMapper(jsonMap)
-    Business(bv(0), bv(1), bv(2), bv(3), bv(4), bv(5), bv(6), bv(7), bv(8), bv(9), bv(10))
+    Business(bv(0).toLong, bv(1), bv(2), bv(3), bv(4), bv(5), bv(6), bv(7), bv(8), bv(9), bv(10))
   }
 }
