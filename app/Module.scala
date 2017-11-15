1c008240a7631ece231ec77ba4bd0339281ff5f5
Badur Chohan <chohab@MacBook-Pro.local>
2017-11-15 10:35:13 +0000

    Added environment variables for circuit breaker configs

--- app/Module.scala
+++ app/Module.scala
@@ -2,7 +2,7 @@ import com.google.inject.AbstractModule
 
 import com.typesafe.config.{ Config, ConfigFactory }
 import play.api.{ Configuration, Environment }
-import config.SbrConfigManager
+import config.BiConfigManager
 
 /**
  * This class is a Guice module that tells Guice how to bind several
@@ -17,7 +17,7 @@ import config.SbrConfigManager
 class Module(environment: Environment, configuration: Configuration) extends AbstractModule {
 
   override def configure() = {
-    val config = SbrConfigManager.envConf(ConfigFactory.load())
+    val config = BiConfigManager.envConf(ConfigFactory.load())
     bind(classOf[Config]).toInstance(config)
   }
 }
--- app/utils/DangerousActor.scala
+++ app/utils/DangerousActor.scala
@@ -1,22 +1,25 @@
 package utils
 
 import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.util.{ Failure, Success, Try }
+
 import akka.pattern.CircuitBreaker
 import akka.actor.{ Actor, ActorLogging }
 
-import scala.concurrent.Future
-import scala.util.{ Failure, Success, Try }
+import com.typesafe.config.{ Config, ConfigFactory }
 
 class DangerousActor extends Actor with ActorLogging {
 
   import context.dispatcher
+  private val config: Config = ConfigFactory.load()
 
   val breaker =
     new CircuitBreaker(
       context.system.scheduler,
-      maxFailures = 5,
-      callTimeout = 1.seconds,
-      resetTimeout = 20.seconds
+      maxFailures = config.getInt("circuit-breaker.failure.threshold"),
+      callTimeout = config.getInt("circuit-breaker.failure.declaration.time").seconds,
+      resetTimeout = config.getInt("circuit-breaker.reset.timeout").seconds
     ).
       onOpen(notifyMe("Open")).
       onClose(notifyMe("Closed")).
