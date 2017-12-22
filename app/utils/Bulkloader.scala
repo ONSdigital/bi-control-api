a7ae839b6b523ee8c3d69c04c042cdd62be21ce7
ONS-Anthony <ChiuA@MacBook-Pro.local>
2017-12-22 09:34:07 +0000

    Updated deprecated HFileOutputFormat to HFileOutputFormat2 and updated readme

--- app/utils/Bulkloader.scala
+++ app/utils/Bulkloader.scala
@@ -1,15 +1,14 @@
 package utils
 
 import java.io.File
+
 import org.apache.spark.sql.SparkSession
 import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat
-
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.client.HTable
 import org.apache.hadoop.hbase.KeyValue
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
-import org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles
-import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat
+import org.apache.hadoop.hbase.mapreduce.{HFileOutputFormat, HFileOutputFormat2, LoadIncrementalHFiles}
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.hbase.mapred.TableOutputFormat
 import org.apache.hadoop.fs.Path
@@ -37,25 +36,22 @@ class Bulkloader() {
       }
     })
 
+    val conf = HBaseConfiguration.create()
+    val tableName = "august"
+    val table = new HTable(conf, tableName)
+
+    conf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
+    val job = Job.getInstance(conf)
+    job.setMapOutputKeyClass(classOf[ImmutableBytesWritable])
+    job.setMapOutputValueClass(classOf[KeyValue])
+    HFileOutputFormat2.configureIncrementalLoadMap(job, table)
+
+    pairs.saveAsNewAPIHadoopFile(savePath, classOf[ImmutableBytesWritable], classOf[KeyValue], classOf[HFileOutputFormat2])
+
+    val bulkLoader = new LoadIncrementalHFiles(conf)
+    bulkLoader.doBulkLoad(new Path(loadPath), table)
+
     pairs.collect()
     ss.stop()
-
-    //    val conf = HBaseConfiguration.create()
-    //    val tableName = "august"
-    //    val table = new HTable(conf, tableName)
-
-    //    conf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
-    //    val job = Job.getInstance(conf)
-    //    job.setMapOutputKeyClass(classOf[ImmutableBytesWritable])
-    //    job.setMapOutputValueClass(classOf[KeyValue])
-    //    HFileOutputFormat.configureIncrementalLoad(job, table)
-
-    //pairs.saveAsHadoopFile(savePath, classOf[String], classOf[String], classOf[MultipleTextOutputFormat[String, String]])
-    //pairs.saveAsNewAPIHadoopFile(savePath, classOf[ImmutableBytesWritable], classOf[KeyValue], classOf[HFileOutputFormat])
-
-    //bulk load
-    //    val bulkLoader = new LoadIncrementalHFiles(conf)
-    //    bulkLoader.doBulkLoad(new Path(loadPath), table)
-    //pairs.collect()
   }
 }
