85a315839aa43c989b17cc5f8d9a02981fb4ccb1
Badur Chohan <BChohan@qa.com>
2018-01-24 09:26:10 +0000

    Feature/cache (#4)
    * Caching response
    
    * Added cache duration as config param
    
    * Cache cleanup
    
    * Scala spark bulkload of Hfile
    
    * Fixed columns and made test bed
    
    * Updated deprecated HFileOutputFormat to HFileOutputFormat2 and updated readme
    
    * Cleanup versions
    
    * Updated write to hfile method
    
    * Added params for bulkloading
    Added local hbase-site.xml for hbase conf
    
    * Refined bulkload process

--- app/utils/Bulkloader.scala
+++ app/utils/Bulkloader.scala
@@ -2,60 +2,58 @@ package utils
 
 import java.io.File
 
-import org.apache.spark.sql.{ Row, SparkSession }
+import org.apache.spark.sql.SparkSession
 import org.apache.hadoop.hbase.{ HBaseConfiguration, KeyValue }
 import org.apache.hadoop.hbase.client.HTable
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.{ HFileOutputFormat2, LoadIncrementalHFiles }
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.hbase.mapred.TableOutputFormat
-import org.apache.hadoop.fs.Path
-import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.fs.{ Path, FileSystem }
 
 class Bulkloader() extends java.io.Serializable {
 
   def loadHbase() {
 
-    val csvFile = new File(s"conf/sample/sbr-2500-ent-ch-data.csv").toURI.toURL.toExternalForm
+    val csvFile = new File(s"conf/sample/fulldata.csv").toURI.toURL.toExternalForm
     val ss = SparkSession.builder().master("local").appName("appName").getOrCreate()
 
     val path = "conf/sample/hfile"
-    val hbasePath = "/usr/local/Cellar/hbase/1.2.6/libexec/conf"
-    val column = "companyname"
+    val id = " CompanyNumber"
+
     val period = "201706"
+    val tableName = "june"
+    val colFamily = "d"
 
     val df = ss.read
       .option("header", true)
       .csv(csvFile)
-      .sort("companynumber")
+      .sort(id)
 
-    val pairs = df.rdd.map(line => convertToKeyValuePairs(line, column, period))
+    val sortedCol = df.columns.sorted
+
+    val pairs = df.rdd.flatMap(line => {
+      val rowKey = s"${line.getAs(id)}~$period"
+      for (i <- sortedCol) yield {
+        val value = if (line.getAs[String](i) == null) "" else line.getAs[String](i)
+        (new ImmutableBytesWritable(rowKey.getBytes()), new KeyValue(rowKey.getBytes(), colFamily.getBytes(), i.getBytes(), value.getBytes()))
+      }
+    })
 
     val conf = HBaseConfiguration.create()
-    val tableName = "june"
     val table = new HTable(conf, tableName)
-
     conf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
-    conf.addResource(new File(s"$hbasePath/hbase-site.xml").toURI.toURL)
+    //conf.setInt("hbase.mapreduce.bulkload.max.hfiles.perRegion.perFamily", 500)
     val job = Job.getInstance(conf)
     job.setMapOutputKeyClass(classOf[ImmutableBytesWritable])
     job.setMapOutputValueClass(classOf[KeyValue])
     HFileOutputFormat2.configureIncrementalLoadMap(job, table)
+    val fs = FileSystem.get(conf)
 
     pairs.saveAsNewAPIHadoopFile(path, classOf[ImmutableBytesWritable], classOf[KeyValue], classOf[HFileOutputFormat2])
     val bulkLoader = new LoadIncrementalHFiles(conf)
     bulkLoader.doBulkLoad(new Path(path), table)
+    fs.delete(new Path(path))
     ss.stop()
-  }
-
-  def convertToKeyValuePairs(line: Row, column: String, period: String): (ImmutableBytesWritable, KeyValue) = {
-    val rowkey = Bytes.toBytes(s"${line(1)}~$period")
-    val cell = new KeyValue(
-      rowkey,
-      Bytes.toBytes("d"),
-      Bytes.toBytes(column),
-      Bytes.toBytes(line.getAs[String](column))
-    )
-    (new ImmutableBytesWritable(rowkey), cell)
   }
 }
